5af9493c98c2621358830148a8ef5a6fffffc7bb
haseebshaikh03 <64073823+haseebshaikh03@users.noreply.github.com>
2025-10-21 13:39:43 +0500

    up

--- Q3/next.config.js
+++ Q3/next.config.js
@@ -1,9 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['example.com'], // Replace with your image domains
-  },
-  env: {
-    CUSTOM_ENV_VARIABLE: process.env.CUSTOM_ENV_VARIABLE, // Example of custom environment variable
-  },
-};
+}
+
+module.exports = nextConfig
